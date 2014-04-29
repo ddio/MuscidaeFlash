fb8c00e64fef4556ad8ecefe45f7a29639cc3334
Ddio Chuang <ddio.chuang@lileesystems.com>
2014-04-30 01:05:07 +0800

    add tmp data, add about page, modify layout

--- js/main.js
+++ js/main.js
@@ -3,13 +3,37 @@
 var ST_URL = 'https://script.google.com/macros/s/AKfycbwFse-INUkS6qvZ2waHiBYe_skn1CBp8TqPPK1C9ksvYfwJyAIW/exec',
 	MAX_N_HOSPITAL = 50;
 
+//ST_URL = 'data/tmp.json';
+
 function ViewModel() {
 
 	this.geoMap = {};
 	this.hospitals = ko.observable([]);
+	this.topFly = ko.computed( function() {
+		if( this.hospitals().length > 0 ) {
+			return this.hospitals()[0];
+		}
+		return { hospital: '' };
+	}, this);
 	this.totalComments = ko.observable(0);
 
 	this.map = new google.maps.Map( document.getElementById( 'map-canvas' ) );
+	this.markers = [];
+	this.mapBoundry = null;
+
+	this.viewTop = function() {
+		if( this.markers.length > 0 ) {
+			this.map.setCenter( this.markers[0].getPosition() );
+			return true;
+		}
+	}
+
+	this.viewAll = function() {
+		if( this.mapBoundry ) {
+			this.map.fitBounds( this.mapBoundry );
+			return true;
+		}
+	}
 
 	$.ajax( ST_URL, {
 		dataType: 'json',
@@ -65,11 +89,7 @@ ViewModel.prototype.initMap = function() {
 			iconSize = _.last( iconSizes );
 		}
 
-		var icon = new google.maps.MarkerImage( 
-				new google.maps.Size( iconSize, iconSize ),
-				new google.maps.Point(0,0)
-			),
-			position = new google.maps.LatLng( item.lat, item.lng ),
+		var position = new google.maps.LatLng( item.lat, item.lng ),
 			marker = new google.maps.Marker({
 				title: item.hospital,
 				position: position,
@@ -77,12 +97,19 @@ ViewModel.prototype.initMap = function() {
 				icon: 'images/marker.'+iconSize+'.png'
 			});
 
+		this.markers.push( marker );
+
 		boundry.extend( position );
+
+		if( i == 0 ) {
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+		}
 
 		//TODO: add click event
 	}, this);
 
 	this.map.fitBounds( boundry );
+	this.mapBoundry = boundry;
 }
 
 $( function() {
